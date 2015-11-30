6e95fb9c97b766df2e1625963904924c17d33050
MarvinKock <marvin_kock@hotmail.de>
2015-11-30 08:56:14 +0100

    Kran speichert die ÜbergabeParameter

--- D47_WPF_Kran/Kran.cs
+++ D47_WPF_Kran/Kran.cs
@@ -11,14 +11,37 @@ namespace D47_WPF_Kran
 {
     class Kran
     {
+        double width = 30.0;
+        double height = 40.0;
+        double topProperty = 100.0;
+        double leftProperty = 0.0;
+        int x1_left;
+        int x2_left;
+        int x1_right;
+        int x2_right;
+        int y1_left;
+        int y2_left;
+        int y1_right;
+        int y2_right;
+        private int xKoordinate;
+        private int yKoordinate;
+
+
         public Kran(int x1, int x2, int y1, int y2, Rectangle kran, Line links, Line rechts)
         {
-            double width = 30.0;
+                 this.x1_left = x1;
+                 this.x2_left = x2;
+                 this.x1_right = x1 + (int)width;
+                 this.x2_right = x2 + (int)width;
+                 this.y1_left = y1;
+                 this.y2_left = y2;
+                 this.y1_right = y1;
+                 this.y2_right = y2;
 
-            links.X1 = x1;
-            links.X2 = x2;
-            links.Y1 = y1;
-            links.Y2 = y2;
+                 links.X1 = this.x1_left;
+                 links.X2 = this.x2_left;
+                 links.Y1 = this.y1_left;
+                 links.Y2 = this.y2_left;
 
             links.Fill = Brushes.Black;
             links.Stroke = Brushes.Gray;
@@ -27,24 +50,46 @@ namespace D47_WPF_Kran
 
 
 
-            rechts.X1 = x1 + width;
-            rechts.X2 = x2 + width;
-            rechts.Y1 = y1;
-            rechts.Y2 = y2;
+            rechts.X1 = this.x1_right;
+            rechts.X2 = this.x2_right;
+            rechts.Y1 = this.y1_right;
+            rechts.Y2 = this.y2_right;
 
             rechts.Fill = Brushes.Black;
             rechts.Stroke = Brushes.Gray;
             rechts.StrokeThickness = 5;
             // this.Children.Add(linie2);
 
-
+            this.leftProperty = x1;
             kran.Fill = Brushes.Black;
-            kran.Width = width;
-            kran.Height = 40.0;
-            kran.SetValue(Canvas.TopProperty, 100.0);
-            kran.SetValue(Canvas.LeftProperty, (double)x1);
+            kran.Width = this.width;
+            kran.Height = this.height;
+            kran.SetValue(Canvas.TopProperty, topProperty);
+            kran.SetValue(Canvas.LeftProperty, leftProperty);
             //this.Children.Add(kran);
         }
+
+
+        public void bewegungXrichtungPositiv()
+        {
+            this.xKoordinate++;
+        }
+
+        public void bewegungXrichtungNegativ()
+        {
+            this.xKoordinate--;
+        }
+
+        public void bewegungYrichtungPositiv()
+        {
+            this.yKoordinate++;
+        }
+
+        public void bewegungYrichtungNegativ()
+        {
+            this.yKoordinate--;
+        }
+
 
 
     }
